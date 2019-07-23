6fe6ae59226f698439e81ab9f30c4c682987a469
alwejdan17 <wejdanalharby.com>
2019-07-23 15:33:48 +0300

    task_04 _create model and objects

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.TextField()
+    opening_time=models.TimeField(null=True, blank=True)
+    closing_time=models.TimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+        
+
+
